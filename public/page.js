ff8d94a7a8a2e833ca309c60bc0fd39f593f0abf
Kyle Muldoon <kylemuldoon15@gmail.com>
2020-04-06 21:32:26 -0700

    Almost done. added text for each slide and it is all working properly. Just need to add slide transitions now.

--- public/page.js
+++ public/page.js
@@ -1,3 +1,6 @@
+//=======================================================
+//          DATE COUNTDOWN
+//=======================================================
 // Set the date we're counting down to
 var countDownDate = new Date("May 10, 2020 17:00:00").getTime();
 
@@ -25,7 +28,38 @@ var x = setInterval(function () {
     }
 }, 1000);
 
+//=======================================================
+//          SLIDESHOW
+//=======================================================
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("my-slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
 
